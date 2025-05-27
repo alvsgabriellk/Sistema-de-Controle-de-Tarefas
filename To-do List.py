d4d86016696f512a3399ceae22e83b8125ec5e8a
alvsgabriellk <cauagabriell81@gmail.com>
2025-05-27 11:42:29 -0300

    add de função de novas tarefas

--- To-do List.py
+++ To-do List.py
@@ -15,6 +15,19 @@ print('''    [ 1 ] | Adicionar tarefas
 print('-='*20)
 opção = int(input('-----> Digite aqui a alternativa:'))
 while opção != 6:
+    if opção == 1:
+        duvida_tarefa_nova = str(input('-----> Deseja adicionar uma tarefa? s/n:')).strip().upper()[0]
+        if duvida_tarefa_nova == 'S':
+            quantidade_tarefas = int(input('Quantas tarefas vai querer adicionar?:'))
+            for tarefa in range(quantidade_tarefas):
+                nova_tarefa = str(input('Nova tarefa:'))
+                lista_tarefas_novas.append(nova_tarefa)
+                status_pendente_tarefa.append(nova_tarefa)
+            print('--> Todas as {} tarefas adicionadas na lista!.'.format(quantidade_tarefas))
+        elif duvida_tarefa_nova == 'N':
+            print('Encerrando programa...')
+            sleep(1.2)
+        print('-'*50)
     print('-=' * 20)
     print('   ------------MENU ABERTO------------')
     print('''    [ 1 ] | Adicionar tarefas
