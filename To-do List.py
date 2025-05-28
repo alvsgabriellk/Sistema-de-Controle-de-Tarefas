86d87249f20c601cd3d8601ef3c16caecd4bff3d
alvsgabriellk <cauagabriell81@gmail.com>
2025-05-28 10:11:05 -0300

    Icrementação da opção 3
    Concluir itens

--- To-do List.py
+++ To-do List.py
@@ -1,7 +1,6 @@
 from time import sleep
 print('Iniciando sistema...')
 sleep(2)
-lista_tarefas = []
 status_pendente_tarefa = []
 status_concluída_tarefa = []
 print('-='*20)
@@ -21,32 +20,37 @@ while opção != 6:
             quantidade_tarefas = int(input('Quantas tarefas vai querer adicionar?:'))
             for tarefa in range(quantidade_tarefas):
                 nova_tarefa = str(input('Nova tarefa:'))
-                lista_tarefas.append(nova_tarefa)
                 status_pendente_tarefa.append(nova_tarefa)
-            print('--> Todas as {} tarefas adicionadas na lista!.'.format(quantidade_tarefas))
+            print('--> Todas as {} tarefas foram adicionadas na lista!'.format(quantidade_tarefas))
         elif duvida_tarefa_nova == 'N':
             print('Encerrando programa...')
             sleep(1.2)
         print('-'*50)
     elif opção == 2:
         print('-'*35)
-        if lista_tarefas:
-            if status_pendente_tarefa:
-                for listagem_pendente in range(len(status_pendente_tarefa)):
-                    print('Tarefa pendente: {}'.format(status_pendente_tarefa[listagem_pendente]))
-            if status_concluída_tarefa:
-                for listagem_concluida in range(len(status_concluída_tarefa)):
-                    print('Tarefa conluída: {}'.format(status_concluída_tarefa[listagem_concluida]))
-        else:
-            print('Nenhuma tarefa encontrada.')
+        if status_pendente_tarefa:
+            for listagem_pendente in range(len(status_pendente_tarefa)):
+                print('Tarefa pendente: {}'.format(status_pendente_tarefa[listagem_pendente]))
+        if status_concluída_tarefa:
+            for listagem_concluida in range(len(status_concluída_tarefa)):
+                print('Tarefa conluída: {}'.format(status_concluída_tarefa[listagem_concluida]))
         print('-'*35)
     elif opção == 3:
+        print('-'*45)
         concluir_tarefa = str(input('Deseja concluir alguma tarefa? s/n:')).strip().upper()[0]
         if concluir_tarefa == 'S':
-            print('PERGUNTAR TAREFA')
+            inspecionar_tarefa = str(input('Digite aqui o nome da tarefa:')).strip()
+            if inspecionar_tarefa in status_pendente_tarefa:
+                index = status_pendente_tarefa.index(inspecionar_tarefa)
+                status_concluída_tarefa.append(status_pendente_tarefa[index])
+                status_pendente_tarefa.remove(status_pendente_tarefa[index])
+                print('Tarefa marcada como conluída!')
+            elif inspecionar_tarefa not in status_pendente_tarefa:
+                print('Tarefa não encontrada.')
         elif concluir_tarefa == 'N':
             print('Encerrando programa...')
             sleep(1.2)
+        print('-' * 45)
     print('-=' * 20)
     print('   ------------MENU ABERTO------------')
     print('''    [ 1 ] | Adicionar tarefas
